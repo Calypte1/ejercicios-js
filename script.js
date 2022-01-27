f18b7c3aaa81063139c02c723f9e3f2d69ba25a9
Calypte1 <98135339+Calypte1@users.noreply.github.com>
2022-01-26 23:45:44 -0600

    Intento 1er ejercicio

--- script.js
+++ script.js
@@ -3,6 +3,25 @@ const ejercicio1 = [
   19, 88, 456, 13, 23, 24,
 ];
 
+const esPrimo = numero => {
+	// Casos especiales
+	if (numero == 0 || numero == 1 || numero == 4) return false;
+	for (let x = 2; x < numero / 2; x++) {
+		if (numero % x == 0) return false;
+	}
+	// Si no se pudo dividir por ninguno de los de arriba, sí es primo
+	return true;
+  }
+
+
+const numerosParaProbar = [3, 100, 85, 64, 46, 39, 40, 30, 20, 24, 25, 6, 10, 54, 82, 71, 67, 77, 17, 29,
+  19, 88, 456, 13, 23, 24];
+
+numerosParaProbar.forEach(numero => {
+	console.log("¿%d es primo? %s", numero, esPrimo(numero));
+
+
+
 const ejercicio2 = [
   {
     nombre: "Gabriel",
